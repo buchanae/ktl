db3ca439841b00a0282c2f6c818ec085068b7e6e
Alex Buchanan <buchanae@ohsu.edu>
2017-08-30 16:32:25 -0700

    Checkpoint some hacky utils useful during smc-het

---
+++ run_seq.go
@@ -0,0 +1,142 @@
+package main
+
+/*
+Lessons:
+- checking the state of multiple, sequential tasks in parallel would help.
+*/
+
+import (
+  "context"
+  "io"
+  "time"
+  "fmt"
+  "os"
+  "github.com/golang/protobuf/jsonpb"
+  "github.com/spf13/cobra"
+)
+
+var restart bool
+
+var runCmd = &cobra.Command{
+  Use: "run [taskdir...]",
+  RunE: func(cmd *cobra.Command, args []string) error {
+    if len(args) == 0 {
+      return cmd.Help()
+    }
+    runSeq(globTasks(args))
+    return nil
+  },
+}
+
+func init() {
+  f := runCmd.Flags()
+  f.BoolVar(&restart, "restart", restart, "Restart failed tasks")
+}
+
+func runSeq(args []string) {
+  cli, err := newTaskClient("funnel_server_1:9090")
+  if err != nil {
+    panic(err)
+  }
+
+  run := runner{cli: cli}
+
+  for _, arg := range args {
+    if run.shouldStart(arg) {
+      fmt.Println("Starting", arg)
+      run.startTask(arg)
+      return
+    }
+  }
+}
+
+type runner struct {
+  cli TaskServiceClient
+}
+
+func (run *runner) shouldStart(arg string) bool {
+  id := loadID(arg)
+
+  if id == "" {
+    return true
+  }
+
+  r, err := run.cli.GetTask(context.Background(), &GetTaskRequest{Id: id})
+  if err != nil && !isNotFound(err) {
+    panic(err)
+  }
+
+  switch r.GetState() {
+  case State_ERROR, State_SYSTEM_ERROR, State_CANCELED:
+    if restart {
+      return true
+    }
+
+  case State_UNKNOWN:
+    return true
+  }
+  return false
+}
+
+func (run *runner) startTask(arg string) {
+
+  f, err := os.Open(arg)
+  if err != nil {
+    panic(err)
+  }
+
+  task, err := loadTask(f)
+  if err != nil {
+    panic(err)
+  }
+
+  r, err := run.cli.CreateTask(context.Background(), task)
+  if err != nil {
+    panic(err)
+  }
+
+  fmt.Println("Created:", r.Id)
+  saveID(arg, r.Id)
+}
+
+
+func saveID(path, id string) {
+  f, err := os.Create(path + ".id")
+  defer f.Close()
+  if err != nil {
+    panic(err)
+  }
+  f.WriteString(id)
+}
+
+
+func waitForTask(ctx context.Context, client TaskServiceClient, id string) error {
+  for {
+    r, err := client.GetTask(ctx, &GetTaskRequest{Id: id})
+    if err != nil {
+      return err
+    }
+
+    switch r.State {
+    case State_ERROR, State_SYSTEM_ERROR:
+      return fmt.Errorf("Task error")
+    case State_CANCELED:
+      return fmt.Errorf("Task canceled")
+    case State_COMPLETE:
+      return nil
+    default:
+      fmt.Println("State:", r.State.String())
+    }
+    time.Sleep(time.Second)
+  }
+}
+
+func loadTask(r io.Reader) (*Task, error) {
+  t := Task{}
+  err := jsonpb.Unmarshal(r, &t)
+  if err != nil {
+    return nil, err
+  }
+  return &t, nil
+}
+
---
+++ status.go
@@ -0,0 +1,251 @@
+package main
+
+/*
+Lessons:
+- checking the state of multiple, sequential tasks in parallel would help.
+*/
+
+import (
+  "context"
+  "strings"
+  "path/filepath"
+  "io/ioutil"
+  "fmt"
+  "os"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+  "github.com/spf13/cobra"
+)
+
+// RootCmd represents the root command
+var rootCmd = &cobra.Command{
+    Use:           "ktl",
+    SilenceErrors: true,
+    SilenceUsage:  true,
+}
+
+func init() {
+  rootCmd.AddCommand(statusCmd)
+  rootCmd.AddCommand(runCmd)
+}
+
+func main() {
+  if err := rootCmd.Execute(); err != nil {
+    fmt.Println(err.Error())
+    os.Exit(1)
+  }
+}
+
+func globTasks(dirs []string) []string {
+  var res []string
+  for _, dir := range dirs {
+    m, _ := filepath.Glob(filepath.Join(dir, "task*.json"))
+    res = append(res, m...)
+  }
+  return res
+}
+
+var statusCmd = &cobra.Command{
+    Use:   "status [task.json ...]",
+    RunE: func(cmd *cobra.Command, args []string) error {
+        if len(args) == 0 {
+            return cmd.Help()
+        }
+        doStatus(globTasks(args))
+        return nil
+    },
+}
+
+
+var statusFlags = struct {
+  running bool
+  queued bool
+  unknown bool
+  initializing bool
+  canceled bool
+  complete bool
+  err bool
+  syserr bool
+  anyerr bool
+  done bool
+
+  last bool
+  latest bool
+
+  base bool
+  path bool
+  id bool
+  state bool
+}{}
+
+
+func init() {
+  f := statusCmd.Flags()
+  f.BoolVar(&statusFlags.running, "running", statusFlags.running, "Running tasks only")
+  f.BoolVar(&statusFlags.queued, "queued", statusFlags.queued, "queued tasks only")
+  f.BoolVar(&statusFlags.unknown, "unknown", statusFlags.unknown, "unknown tasks only")
+  f.BoolVar(&statusFlags.initializing, "initializing", statusFlags.initializing, "initializing tasks only")
+  f.BoolVar(&statusFlags.canceled, "canceled", statusFlags.canceled, "canceled tasks only")
+  f.BoolVar(&statusFlags.complete, "complete", statusFlags.complete, "complete tasks only")
+  f.BoolVar(&statusFlags.err, "err", statusFlags.err, "err tasks only")
+  f.BoolVar(&statusFlags.syserr, "syserr", statusFlags.syserr, "syserr tasks only")
+  f.BoolVar(&statusFlags.anyerr, "anyerr", statusFlags.anyerr, "anyerr tasks only")
+  f.BoolVar(&statusFlags.done, "done", statusFlags.done, "done tasks only")
+
+  f.BoolVar(&statusFlags.last, "last", statusFlags.last, "last tasks only")
+  f.BoolVar(&statusFlags.latest, "latest", statusFlags.latest, "Latest tasks only")
+
+  f.BoolVar(&statusFlags.base, "base", statusFlags.base, "include base field")
+  f.BoolVar(&statusFlags.path, "path", statusFlags.path, "include path field")
+  f.BoolVar(&statusFlags.id, "id", statusFlags.id, "include id field")
+  f.BoolVar(&statusFlags.state, "state", statusFlags.state, "include state field")
+}
+
+type row struct {
+  ID string
+  BasePath string
+  Path string
+  State State
+  IsLast bool
+}
+
+func doStatus(args []string) {
+
+  // Default column config
+  if !statusFlags.id && !statusFlags.base && !statusFlags.path && !statusFlags.state {
+    statusFlags.id = true
+    statusFlags.base = true
+    statusFlags.path = true
+    statusFlags.state = true
+  }
+
+  cli, err := newTaskClient("funnel_server_1:9090")
+  if err != nil {
+    panic(err)
+  }
+
+  if statusFlags.last {
+    args = args[len(args) - 1:]
+  }
+
+  var rows []*row
+
+  base := filepath.Dir(args[0])
+
+  for i, arg := range args {
+    id := loadID(arg)
+    r := row{
+      ID: id,
+      BasePath: base,
+      Path: arg,
+      // TODO assumes linear sequence
+      IsLast: i == len(args) - 1,
+    }
+    rows = append(rows, &r)
+
+    if id != "" {
+      resp, err := cli.GetTask(context.Background(), &GetTaskRequest{Id: id})
+      if err != nil && !isNotFound(err) {
+        panic(err)
+      }
+
+      r.State = resp.GetState()
+    }
+  }
+
+  var filtered []*row
+
+  for i, r := range rows {
+    switch {
+    case statusFlags.latest && (r.State == State_UNKNOWN || r.State == State_QUEUED):
+    case statusFlags.latest && i < len(rows) - 1 && rows[i + 1].State != State_UNKNOWN:
+    default:
+      filtered = append(filtered, r)
+    }
+  }
+
+  for _, r := range filtered {
+    var cols []string
+
+    if statusFlags.running && r.State != State_RUNNING {
+      continue
+    }
+    if statusFlags.complete && r.State != State_COMPLETE {
+      continue
+    }
+    if statusFlags.queued && r.State != State_QUEUED {
+      continue
+    }
+    if statusFlags.unknown && r.State != State_UNKNOWN {
+      continue
+    }
+    if statusFlags.initializing && r.State != State_INITIALIZING {
+      continue
+    }
+    if statusFlags.err && r.State != State_ERROR {
+      continue
+    }
+    if statusFlags.syserr && r.State != State_SYSTEM_ERROR {
+      continue
+    }
+    if statusFlags.anyerr && r.State != State_SYSTEM_ERROR && r.State != State_ERROR {
+      continue
+    }
+    if statusFlags.canceled && r.State != State_CANCELED {
+      continue
+    }
+    if statusFlags.done && !isDone(r.State) {
+      continue
+    }
+
+    // TODO cols should match the order of flags
+    if statusFlags.id {
+      cols = append(cols, fmt.Sprintf("%20s", r.ID))
+    }
+    if statusFlags.base {
+      cols = append(cols, r.BasePath)
+    }
+    if statusFlags.path {
+      cols = append(cols, r.Path)
+    }
+    if statusFlags.state {
+      cols = append(cols, r.State.String())
+    }
+
+    fmt.Println(strings.Join(cols, "\t"))
+  }
+}
+
+func isDone(s State) bool {
+	return s == State_COMPLETE || s == State_ERROR || s == State_SYSTEM_ERROR ||
+		s == State_CANCELED
+}
+
+func isNotFound(err error) bool {
+  s, ok := status.FromError(err)
+  return ok && s.Code() == codes.NotFound
+}
+
+func loadID(path string) string {
+  b, err := ioutil.ReadFile(path + ".id")
+  if os.IsNotExist(err) {
+    return ""
+  }
+  if err != nil {
+    panic(err)
+  }
+  return strings.Trim(string(b), "\n")
+}
+
+func newTaskClient(addr string) (TaskServiceClient, error) {
+	conn, err := grpc.Dial(
+    addr,
+		grpc.WithInsecure(),
+		grpc.WithBlock(),
+	)
+	if err != nil {
+		return nil, err
+	}
+  return NewTaskServiceClient(conn), nil
+}
