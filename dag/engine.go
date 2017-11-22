a3898defec32dd1c1e89a6b16256578472f33cb4
Kyle Ellrott <kellrott@gmail.com>
2017-11-22 10:37:54 -0800

    Working DAG unit test

--- dag/engine.go
+++ dag/engine.go
@@ -1,103 +1,137 @@
-
 package dag
 
 import (
-  //"log"
+	"log"
+	"sync"
 )
 
 type DAGEngine interface {
-  Start(chan Event) chan Event
+	Start(chan Event) chan Event
+	ActiveCount() int
 }
 
 type MemoryDAG struct {
-  steps  map[string]Step
-  states map[string]EventType
-  deps   map[string][]string
-  rev    map[string][]string
-  out    chan Event
+	steps       map[string]Step
+	states      map[string]EventType
+	deps        map[string][]string
+	rev         map[string][]string
+	out         chan Event
+	state_mutex sync.Mutex
 }
 
+func (self *MemoryDAG) ActiveCount() int {
+	self.state_mutex.Lock()
+	i := 0
+	for _, x := range self.states {
+		switch x {
+		case EventType_NEW, EventType_READY, EventType_RUNNING:
+			i += 1
+		}
+	}
+	self.state_mutex.Unlock()
+	return i
+}
 
 func (self *MemoryDAG) process_NEW(i Event) {
-  //log.Printf("%#v", i)
-  self.steps[i.StepId] = Step{StepId:i.StepId, Depends:i.Depends}
-  depends := []string{}
-  in_error := false
-  for _, d := range i.Depends {
-    if x, ok := self.states[d]; ok {
-      switch x {
-      case EventType_NEW, EventType_READY:
-        depends = append(depends, d)
-      case EventType_UNKNOWN, EventType_FAILURE, EventType_CANCELED:
-        in_error = true
-      case EventType_SUCCESS:
-      }
-    }
-  }
-  if in_error {
-    self.states[i.StepId] = EventType_FAILURE
-    self.out <- Event{StepId:i.StepId, Event:EventType_FAILURE}          
-  } else {
-    if len(depends) > 0 {
-      self.states[i.StepId] = EventType_NEW
-      self.deps[i.StepId] = depends
-      for _, j := range depends {
-        if x, ok := self.rev[j]; ok {
-          self.rev[j] = append(x, i.StepId)
-        } else {
-          self.rev[j] = []string{i.StepId}
-        }
-      }
-    } else {
-      self.states[i.StepId] = EventType_READY
-      self.out <- Event{StepId:i.StepId, Event:EventType_READY}
-    }
-  }
+	//log.Printf("%#v", i)
+	self.steps[i.StepId] = Step{StepId: i.StepId, Depends: i.Depends}
+	depends := []string{}
+	in_error := false
+	for _, d := range i.Depends {
+		self.state_mutex.Lock()
+		if x, ok := self.states[d]; ok {
+			switch x {
+			case EventType_NEW, EventType_READY, EventType_RUNNING:
+				depends = append(depends, d)
+			case EventType_UNKNOWN, EventType_FAILURE, EventType_CANCELED:
+				in_error = true
+			case EventType_SUCCESS:
+			}
+		} else {
+			log.Printf("Depends %s Not Found: %s", i.StepId, d)
+		}
+		self.state_mutex.Unlock()
+	}
+	if in_error {
+		self.state_mutex.Lock()
+		self.states[i.StepId] = EventType_FAILURE
+		self.state_mutex.Unlock()
+		self.out <- Event{StepId: i.StepId, Event: EventType_FAILURE}
+	} else {
+		if len(depends) > 0 {
+			log.Printf("Depends %s : %s", i.StepId, depends)
+			self.state_mutex.Lock()
+			self.states[i.StepId] = EventType_NEW
+			self.state_mutex.Unlock()
+			self.deps[i.StepId] = depends
+			for _, j := range depends {
+				if x, ok := self.rev[j]; ok {
+					self.rev[j] = append(x, i.StepId)
+				} else {
+					self.rev[j] = []string{i.StepId}
+				}
+			}
+		} else {
+			self.state_mutex.Lock()
+			self.states[i.StepId] = EventType_RUNNING
+			self.state_mutex.Unlock()
+			self.out <- Event{StepId: i.StepId, Event: EventType_READY}
+		}
+	}
 }
 
-
 func remove(in []string, s string) []string {
-  out := make([]string, 0, len(in))
-  for _, x := range in {
-    if x != s {
-      out = append(out, x)
-    }
-  }
-  return out
+	out := make([]string, 0, len(in))
+	for _, x := range in {
+		if x != s {
+			out = append(out, x)
+		}
+	}
+	return out
 }
 
 func (self *MemoryDAG) process_SUCCESS(i Event) {
-  if x, ok := self.rev[i.StepId]; ok {
-    for _, d := range x {
-      nd := remove(self.deps[d], i.StepId)
-      if len(nd) == 0 {
-        delete(self.deps, d)
-        self.out <- Event{StepId:i.StepId, Event:EventType_READY}
-      } else {
-        self.deps[d] = nd
-      }
-    }
-    delete(self.rev, i.StepId)
-  }
+	log.Printf("success: %s", i.StepId)
+	//log.Printf("rev: %s %s", i.StepId, self.rev)
+	if x, ok := self.rev[i.StepId]; ok {
+		log.Printf("Success: %s resolves dependency for %s", i.StepId, x)
+		for _, d := range x {
+			nd := remove(self.deps[d], i.StepId)
+			if len(nd) == 0 {
+				delete(self.deps, d)
+				self.states[d] = EventType_RUNNING
+				log.Printf("Starting: %s", d)
+				self.out <- Event{StepId: d, Event: EventType_READY}
+			} else {
+				self.deps[d] = nd
+			}
+		}
+		delete(self.rev, i.StepId)
+	}
+	self.state_mutex.Lock()
+	self.states[i.StepId] = EventType_SUCCESS
+	self.state_mutex.Unlock()
 }
 
 func (self *MemoryDAG) Start(input chan Event) chan Event {
-  self.steps = map[string]Step{}
-  self.states = map[string]EventType{}
-  self.rev = map[string][]string{}
-  self.deps = map[string][]string{}
-  self.out = make(chan Event, 10)
-  
-  go func() {
-    defer close(self.out)
-    for i:= range input {
-      switch i.Event {
-      case EventType_NEW:
-        self.process_NEW(i)
-      case EventType_SUCCESS:
-        self.process_SUCCESS(i)
-      }
-    }
-  }()
-  return self.out
+	self.steps = map[string]Step{}
+	self.states = map[string]EventType{}
+	self.rev = map[string][]string{}
+	self.deps = map[string][]string{}
+	self.out = make(chan Event, 10)
+
+	go func() {
+		defer close(self.out)
+		for i := range input {
+			switch i.Event {
+			case EventType_NEW:
+				self.process_NEW(i)
+			case EventType_SUCCESS:
+				self.process_SUCCESS(i)
+			default:
+				log.Printf("Unknown Event")
+			}
+		}
+	}()
+	return self.out
 }
--- tests/dag_test.go
+++ tests/dag_test.go
@@ -1,80 +1,80 @@
-
-
 package dag
 
 import (
-  "log"
-  //"sync"
-  "fmt"
-  "time"
-  "math/rand"
-  "testing"
-  "github.com/ohsu-comp-bio/ktl/dag"
+	"log"
+	//"sync"
+	"fmt"
+	"github.com/ohsu-comp-bio/ktl/dag"
+	"math/rand"
+	"testing"
+	"time"
 )
 
 func choose(in []string, count int) []string {
-  t := make(map[int32]bool, count)
-  for ; len(t) < count && len(t) < len(in) ; {
-    t[ rand.Int31n(int32(len(in))) ] = true
-  }
-  out := make([]string, 0, count)
-  for i := range t {
-    out = append(out, in[i])
-  }
-  return out
+	t := make(map[int32]bool, count)
+	for len(t) < count && len(t) < len(in) {
+		t[rand.Int31n(int32(len(in)))] = true
+	}
+	out := make([]string, 0, count)
+	for i := range t {
+		out = append(out, in[i])
+	}
+	return out
 }
 
-var STEP_COUNT int = 2000
+var STEP_COUNT int = 10000
 
 func TestRun(t *testing.T) {
-  rand.Seed(time.Now().UnixNano())
+	rand.Seed(time.Now().UnixNano())
 
-  in_events := make(chan dag.Event, 100)
+	in_events := make(chan dag.Event, 100)
 
-  d := dag.MemoryDAG{}
+	d := dag.MemoryDAG{}
 
-  out_events := d.Start(in_events)
+	out_events := d.Start(in_events)
 
-  quit := make(chan bool)
+	quit := make(chan bool)
 
-  //Create Job requests
-  go func() {
-    step_ids := []string{}
-    for i := 0; i < STEP_COUNT; i++ {
-      s := fmt.Sprintf("event_%d", i)
-      step_ids = append(step_ids, s)
-      depends := []string{}
-      if i > 2 {
-        dcount := int(rand.Int31n(int32(i/2))) % 7
-        depends = choose(step_ids, dcount)
-      }
-      in_events <- dag.Event{StepId:s,Event:dag.EventType_NEW,Depends:depends}
-      time.Sleep( time.Duration(rand.Int63n(100)) * time.Microsecond)
-    }
-    close(in_events)
-  }()
-  
-  //Consume events
-//  jobs := sync.Map{}
-  job_quit := false
-  go func() {
-    //defer close(job)
-    for i := range out_events {
-      fmt.Printf("Out: %s\n", i)
-      if i.Event == dag.EventType_READY {
-        //jobs[i.StepId] = true
-      }
-    }
-    job_quit = true
-    quit <- true
-  }()
-  
-  go func() {
-    for job_quit {
-      
-    }
-  }()
+	//Create Job requests
+	go func() {
+		step_ids := []string{}
+		for i := 0; i < STEP_COUNT; i++ {
+			s := fmt.Sprintf("event_%d", i)
+			step_ids = append(step_ids, s)
+			depends := []string{}
+			if i > 2 {
+				dcount := int(rand.Int31n(int32((i-1)/2))) % 7
+				depends = choose(step_ids, dcount)
+			}
+			in_events <- dag.Event{StepId: s, Event: dag.EventType_NEW, Depends: depends}
+			time.Sleep(time.Duration(rand.Int63n(100)) * time.Microsecond)
+		}
+		for d.ActiveCount() > 0 {
+			time.Sleep(500 * time.Microsecond)
+			log.Printf("ActiveCount: %d", d.ActiveCount())
+		}
+		close(in_events)
+	}()
 
-  log.Printf("%#v", d)
-  <- quit
+	//Consume events
+	go func() {
+		//defer close(job)
+		for i := range out_events {
+			log.Printf("Event: %s\n", i)
+			switch i.Event {
+			case dag.EventType_READY:
+				go func(step_id string) {
+					time.Sleep(time.Duration(rand.Int63n(1000)) * time.Microsecond)
+					//log.Printf("Complete: %s", step_id)
+					in_events <- dag.Event{StepId: step_id, Event: dag.EventType_SUCCESS}
+				}(i.StepId)
+			default:
+				log.Printf("Unknown Event: %s", i.Event)
+			}
+		}
+		quit <- true
+	}()
+
+	log.Printf("%#v", d)
+	<-quit
 }
