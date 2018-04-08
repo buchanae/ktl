b7fcd7b9b33389379239579e9e27112e050a03c3
Alex Buchanan <buchanae@ohsu.edu>
2018-04-08 13:35:56 -0700

    enumer

--- mode.go
+++ mode.go
@@ -1,9 +1,5 @@
 package ktl
 
-import (
-	"fmt"
-)
-
 const (
 	// BestEffort mode will run as many steps as possible; any steps which
 	// are not blocked by upstream errors will be run.
@@ -17,33 +13,4 @@ const (
 // Mode determines how the execution engine will handle errors.
 type Mode int
 
-// String returns the string representation of Mode: BestError, FailFast, etc.
-func (m *Mode) String() string {
-	switch *m {
-	case BestEffort:
-		return "BestEffort"
-	case FailFast:
-		return "FailFast"
-	}
-	return "Unknown"
-}
-
-// MarshalText marshals the mode to text, which enables JSON to use
-// the string version instead of an int.
-func (m *Mode) MarshalText() ([]byte, error) {
-	return []byte(m.String()), nil
-}
-
-// UnmarshalText unmarshals the mode from text, which enables JSON to use
-// the string version instead of an int.
-func (m *Mode) UnmarshalText(b []byte) error {
-	switch string(b) {
-	case "BestEffort":
-		*m = BestEffort
-	case "FailFast":
-		*m = FailFast
-	default:
-		return fmt.Errorf(`unknown Mode "%s"`, string(b))
-	}
-	return nil
-}
+//go:generate enumer -type=Mode -text
